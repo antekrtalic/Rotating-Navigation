b1115fc686b2a185e5b5f2772680cc2bb9bb9695
Ante Dragoje <antekrtalic2@gmail.com>
2021-06-07 15:31:52 +0200

    Making changes on navbar div

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const btn = document.querySelector('.trans');
 const main = document.getElementById('main');
+const content = document.querySelector('.content');
 
 btn.addEventListener('click', () => {
-    btn.style.transform = 'rotate(-25deg)';
+    content.style.transformOrigin = 'top left';
+    content.style.transform = 'rotate(-25deg)';
+    content.style.transition = '2s linear all';
 })
