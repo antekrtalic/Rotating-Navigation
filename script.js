f620cc681c08b80f882c34e78b7230012f78225f
Ante Dragoje <antekrtalic2@gmail.com>
2021-06-07 09:07:46 +0200

    Trying to make rotation button for page

---
+++ script.js
@@ -0,0 +1,6 @@
+const btn = document.querySelector('.trans');
+const main = document.getElementById('main');
+
+btn.addEventListener('click', () => {
+    btn.style.transform = 'rotate(-25deg)';
+})
