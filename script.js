616b46ca8463e1012765535895eb33f68412009a
Ante Dragoje <antekrtalic2@gmail.com>
2021-06-10 09:48:26 +0200

    Trying to fix rotation on my div content to rotate nicely

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const main = document.getElementById('main');
 const content = document.querySelector('.content');
 
 btn.addEventListener('click', () => {
-    content.style.transformOrigin = 'top left';
+    
     content.style.transform = 'rotate(-25deg)';
+    content.style.transformOrigin = '0 0';
+    
     content.style.transition = '2s linear all';
 })
