057234f60fd7a66dafd0be689566c3d4b1112ae3
Ante Dragoje <antekrtalic2@gmail.com>
2021-06-11 15:54:02 +0200

    Fixed my rotation button project and with that my project is done

--- script.js
+++ script.js
@@ -4,10 +4,17 @@ const content = document.querySelector('.content');
 
 btn.addEventListener('click', () => {
     
-    content.style.transform = 'rotate(-20deg) translate(212px, -240px)';
-    content.style.transition = '0.4s ease-in';
-    content.classList.add('active');
-    btn.style.transform = 'rotate(-180deg)';
-    btn.style.transition = '1s linear';
+    if (content.classList.contains('active')) {
+        content.style.transform = 'rotate(0deg) translate(0, 0)';
+        content.style.transition = '0.4s ease-in';
+        content.classList.remove('active');
+        btn.style.transform = 'rotate(0deg)';
+        btn.style.transition = '1s linear';
+    } else {
+        content.style.transform = 'rotate(-20deg) translate(212px, -240px)';
+        content.style.transition = '0.4s ease-in';
+        content.classList.add('active');
+        btn.style.transform = 'rotate(-180deg)';
+        btn.style.transition = '1s linear';
+    }
 })
-
